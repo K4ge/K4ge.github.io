85718fdc5e100727293b483b55058eee737794c9
K4ge <779938689@qq.com>
2019-07-20 22:08:32 +0800

    Site updated: 2019-07-20 22:08:22

--- others/create.py
+++ others/create.py
@@ -16,25 +16,33 @@ def unzip_file(zip_src, dst_dir):
 		fz = zipfile.ZipFile(zip_src, 'r')
 		for file in fz.namelist():
 			fz.extract(file, dst_dir)
+		return True
 	else:
 		print('This is not zip')
+		return False
 
 zsrc = "D:/hexo/source/others/%s.zip" % filename
 dsrc = "D:/hexo/source/others/test"
 
-unzip_file(zsrc,dsrc)
-for f in os.listdir(dsrc):
-	if 'md' in f:
-		filename = f
+flag = unzip_file(zsrc,dsrc)
+if flag:
+	for f in os.listdir(dsrc):
+		if 'md' in f:
+			filename = f
+			path = "D:/hexo/source/others/test/%s" % filename
+else:
+	path = "D:/hexo/source/others/"+filename+'.md'
+
 
 #markdown文档处理
+title_pattern = re.compile(r'##.(.*)')
 cat_pattern = re.compile(r'@\((.*)\)')
 tag_pattern = re.compile(r'\[(.*?)\]')
 img_pattren = re.compile(r'!\[Alt text\]\(./')
 img_pattren2 = re.compile(r'!\[.*\]\(\.\/(.*)\)')
-
-with open("D:/hexo/source/others/test/%s" % filename,'r',encoding='UTF-8') as fp:
+with open(path,'r',encoding='UTF-8') as fp:
 	s = fp.read()
+	title = re.findall(title_pattern,s.split('\n')[0])[0]
 	time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	cat = re.findall(cat_pattern,s.split('\n')[1])
 	tag = re.findall(tag_pattern,s.split('\n')[1])
@@ -54,21 +62,22 @@ categories:
 - %s
 tags:
 %s
----''' % (filename[:-3],time,cat[0],tags)
+---''' % (title,time,cat[0],tags)
 
-	result = desc + '\n'+'\n'.join(s.split('\n')[2:]).replace('##','#')
+	result = desc + '\n'+'\n'.join(s.split('\n')[2:]).replace('###','##')
 	# print(result)
-	with open("D:/hexo/source/_posts/%s" % filename,'w',encoding="UTF-8") as f:
+	with open("D:/hexo/source/_posts/%s.md" % title,'w',encoding="UTF-8") as f:
 		f.write(result)
 
 ## 图片处理
 
 # print(imgs)
-for i in imgs:
-	try:
-		shutil.move(dsrc+'/'+i,'D:/hexo/source/img')
-	except:
-		pass
-shutil.rmtree(dsrc)
+if flag:
+	for i in imgs:
+		try:
+			shutil.move(dsrc+'/'+i,'D:/hexo/source/img')
+		except:
+			pass
+	shutil.rmtree(dsrc)
 
 print('Markdown create in hexo successfuly :)')
